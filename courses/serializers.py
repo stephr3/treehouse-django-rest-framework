0e7a9ba117483edda7ec460354949a3f15637ce2
Stephanie Gurung <stephr3@gmail.com>
2016-09-27 19:34:13 -0700

    init model serializers

---
+++ courses/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from . import models
+
+class ReviewSerialize(serializers.ModelSerializer):
+    class Meta:
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+        fields = (
+            'id',
+            'course',
+            'name',
+            'email',
+            'review',
+            'rating',
+            'created_at'
+        )
+        model = models.Review
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Course
+        fields = (
+            'id',
+            'title',
+            'url'
+        )
