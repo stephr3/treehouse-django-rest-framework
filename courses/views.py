90b3808fd1bae267c679ddd61a2d787bb269fa15
Stephanie Gurung <stephr3@gmail.com>
2016-09-27 20:09:54 -0700

    allow admin to create a new course in list create courses view

--- courses/views.py
+++ courses/views.py
@@ -1,11 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from . import models
 from . import serializers
 
-class ListCourse(APIView):
+class ListCreateCourse(APIView):
     def get(self, request, format=None):
         courses = models.Course.objects.all()
         serializer = serializers.CourseSerializer(courses, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = serializers.CourseSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
